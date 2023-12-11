9192f75b07dd0eec20c1d8e23ebfeb3c138b95b9
daonhatviet <daonhatviet2912@gmail.com>
2023-12-11 23:16:27 +0700

    done import export

--- src/app/admin/components/department/department.component.ts
+++ src/app/admin/components/department/department.component.ts
@@ -14,10 +14,15 @@ export class DepartmentComponent implements OnInit {
     fomartDataApiId: any = [];
     DepartmentCode: string = '';
     DepartmentName: string = '';
-    Description: string ='';
+    Descriptions: string ='';
     showModalFlag: boolean = false;
     modalType: 'create' | 'update' = 'create';
+    modelTypeMessage: 'delete' | 'error' = 'delete';
     departmentId: number = 0;
+    departmentIdDelete: number = 0;
+    departmentCode: string | null = null;
+    message: string | null = null;
+    searchKeyWord: string | null = null;
 
     ngOnInit(): void {
         this.getAllDepartment();
@@ -28,6 +33,22 @@ export class DepartmentComponent implements OnInit {
         document.querySelector('.manager__modal')?.classList.add('active');
         document.querySelector('.manager__modal-content')?.classList.add('scale-up-center');
         this.modalType = type;
+        if(type == 'create') {
+            this.resetInput();
+        }
+    }
+
+    //show modal confirm xóa 
+    showConfirmDialog(type: 'delete' | 'error') {
+        document.querySelector('.manager__alert')?.classList.add('active');
+        document.querySelector('.manager__alert-content')?.classList.add('scale-up-center');
+        this.modelTypeMessage = type;
+    }
+
+    //close modal confirm xóa
+    closeConfirmDialog() {        
+        document.querySelector('.manager__alert')?.classList.remove('active');
+        document.querySelector('.manager__alert-content')?.classList.remove('scale-up-center');
     }
 
     //Hide Modal
@@ -36,12 +57,18 @@ export class DepartmentComponent implements OnInit {
         document.querySelector('.manager__modal-content')?.classList.remove('scale-up-center');
     }
 
+    //reset inpu Modal
+    resetInput(){
+        this.DepartmentCode = '';
+        this.DepartmentName = '';
+        this.Descriptions = '';
+    }
+
+    //click edit
     editModalDepartment(Id: number) {
         this.showModalDepartment('update');
         this.getById(Id);
         console.log(this.fomartDataApiId);
-        
-
     }
 
     //call api get all 
@@ -49,6 +76,8 @@ export class DepartmentComponent implements OnInit {
         this.departmentService.getList().subscribe((res: any[]) => {
             this.dataDepartment = res;
             this.fomartDataApi = this.dataDepartment.data;
+            console.log(this.fomartDataApi);
+            
         })
     }
 
@@ -60,6 +89,7 @@ export class DepartmentComponent implements OnInit {
         })
     }
 
+    //submit form
     submitForm(){
         if (this.modalType === 'create') {
             // Thực hiện xử lý tạo mới
@@ -78,25 +108,97 @@ export class DepartmentComponent implements OnInit {
         let body = {
             DepartmentCode : this.DepartmentCode,
             DepartmentName: this.DepartmentName,
-            Descriptions: this.Description
+            Descriptions: this.Descriptions
         }
         
-        this.departmentService.postDepartment(body).subscribe(res => {
+        this.departmentService.postDepartment(body).subscribe((res) => {
             this.closeModalDepartment();
             this.getAllDepartment();
+        }, (error) => {
+            if(error.error){
+                this.showConfirmDialog('error')
+                this.message = error.error.message; 
+            }
         })
         
     }
 
+    //call api update
     updateDepartment() {
         let body = {
             DepartmentCode : this.fomartDataApiId.DepartmentCode,
             DepartmentName: this.fomartDataApiId.DepartmentName,
-            Descriptions: this.fomartDataApiId.Description
+            Descriptions: this.fomartDataApiId.Descriptions
         }
-        console.log(body);
+        let id = this.fomartDataApiId.Id;
         
-        
+        this.departmentService.putDepartment(id, body).subscribe((res) => {
+            this.getAllDepartment();            
+        }, (err) => {
+            if(err.error){
+                this.showConfirmDialog('error')
+                this.message = err.error.message; 
+            }
+        })
     }
     
+    //click delete
+    deleteDepartment(Id: number, Code: string){
+        this.showConfirmDialog('delete')
+        this.departmentIdDelete = Id;
+        this.departmentCode = Code;
+        
+    }
+
+    //call api delete
+    deleteConfirm() {        
+        this.departmentService.delelte(this.departmentIdDelete).subscribe((res) => {
+            if(res.message == "success"){
+                this.getAllDepartment();
+                this.closeConfirmDialog();
+            }
+        }, (err) => {
+            if(err.error){
+                this.showConfirmDialog('error')
+                this.message = err.error.message; 
+            }
+        });
+    }
+
+    //handle file upload
+    handleFileInput(event: any) {
+        const files = event.target.files;
+        
+        const formData = new FormData();
+        formData.append('file', files[0]);
+        
+        this.departmentService.importDepartment(formData).subscribe((res) => {
+            this.getAllDepartment();
+        });
+    }
+
+    //export file
+    exportDepartment(){
+        this.departmentService.exportDepartments().subscribe((res) => {
+            this.downloadFile(res, 'department.xlsx');  
+        });      
+    }
+
+    searchDepartment() {
+        console.log(this.searchKeyWord);
+        
+    }
+
+    //download files
+    private downloadFile(data: Blob, filename: string) {
+        const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }
 }
--- src/app/admin/service/departmentService/department.service.ts
+++ src/app/admin/service/departmentService/department.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -27,6 +27,34 @@ export class DepartmentService {
 		return this.http.post(`${this.baseURL}/department/create`, request);
 	}
 
+	putDepartment(id: number, request: any): Observable<any> {
+		return this.http.put(`${this.baseURL}/department/update/${id}`, request);
+	}
+
+	delelte(id: number): Observable<any> {
+		return this.http.delete(`${this.baseURL}/department/delete/${id}`);
+	}
+
+	searchDepartment(request: any) : Observable<any> {
+		return this.http.post(`${this.baseURL}/department/search`, request);
+	}
+
+	importDepartment(request: any): Observable<any>{
+		return this.http.post(`${this.baseURL}/department/import`, request);
+	}
+
+	exportDepartments() : Observable<any>{
+		const headers = new HttpHeaders({
+			'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+			'Accept': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+		});
+		return this.http.get(`${this.baseURL}/department/export`, {
+            headers,
+            responseType: 'arraybuffer'
+        });
+	}
+
+	
 
 
 }
